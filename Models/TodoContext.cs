b66ad73ea7d93ccce21e00de3cecdea91a0f071b
csandun <csandun.personal@gmail.com>
2022-11-02 23:22:43 +0530

    starter code with is delete checked in where clauses

---
+++ Models/TodoContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CSandunBlogQueryFilterEfCoreApi.Models;
+
+public class TodoContext : DbContext
+{
+    public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>()
+            .Property(o => o.Name)
+            .IsRequired();
+        modelBuilder.Entity<TodoItem>().Property(o => o.IsDelete)
+            .HasDefaultValue(0)
+            .IsRequired();
+
+        modelBuilder.Entity<TodoItem>()
+            .HasData(new TodoItem()
+                {
+                    Id = 1,
+                    Name = "Write blog post for query filter.",
+                    IsComplete = true
+                },
+                new TodoItem()
+                {
+                    Id = 2,
+                    Name = "Create personal web site.",
+                    IsComplete = true
+                },
+                new TodoItem()
+                {
+                    Id = 3,
+                    Name = "Deleted todo item",
+                    IsComplete = true
+                });
+    }
+}
